ac0125df564e6c2ed208308cdb5ca37c32ee96c4
shegsteham <oluwasegunadepoju@gmail.com>
2019-05-30 19:13:30 +0100

    chore: Create Develop branch
    - Create Navbar for authenticated and un-authenticated user
    - update readme
    
    [finishes]

---
+++ src/app/core/nav-bar-auth/nav-bar-auth.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavBarAuthComponent } from './nav-bar-auth.component';
+
+describe('NavBarAuthComponent', () => {
+  let component: NavBarAuthComponent;
+  let fixture: ComponentFixture<NavBarAuthComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavBarAuthComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavBarAuthComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
