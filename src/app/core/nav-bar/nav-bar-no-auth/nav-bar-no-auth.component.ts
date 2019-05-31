cb84cedbb8f87e2bb4962bef7570bae7180335da
shegsteham <oluwasegunadepoju@gmail.com>
2019-05-31 13:34:48 +0100

    feat(NavBar): Implement Navbar
    - implement navbar for authenticated users
    - implement navBar for unauthenticated users
    
    [finishes]

---
+++ src/app/core/nav-bar/nav-bar-no-auth/nav-bar-no-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-no-auth',
+  templateUrl: './nav-bar-no-auth.component.html',
+  styleUrls: ['./nav-bar-no-auth.component.scss']
+})
+export class NavBarNoAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
